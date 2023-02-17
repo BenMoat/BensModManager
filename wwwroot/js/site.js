9f3082655838983f4d9a0a046a6da5d8dd3b666d
Ben Moat <benmoat@hotmail.co.uk>
2023-02-17 18:10:30 +0000

    Sorted out the popup bindings and further removed FCS references

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,8 +8,7 @@ $(document).ready(function () {
     });
 });
 
-//Initialise the dropdown arrays -- this is NOT a future-proof implementation. Needs to be a LINQ SELECT DISTINCT query in the controller
-
+//Set array of mod types
 let ModTypes = [
     { value: "Performance", name: "Performance" },
     { value: "Interior", name: "Interior" },
@@ -48,80 +47,18 @@ showInPopup = (url, title) => {
                 handle: ".modal-header"
             });
 
-            //Load Vehicle Make dropdown
-            var makeCurrentValue = document.getElementById("makeValue").value;
-            new TomSelect('#select-make', {
-                options: [vehicleMakes],
-                items: [makeCurrentValue],
-                placeholder: 'Make',
-                labelField: 'name',
-                searchField: ['name'],
-                openOnFocus: true,
-                highlight: false,
-                hideSelected: true,
-                selectOnTab: true,
-                diacritics: true
-            });
-
-            //Load Paint Type dropdown
+            //Load Mod Type dropdown
             var ModTypeCurrentValue = document.getElementById("ModTypeValue").value;
             new TomSelect('#select-ModType', {
                 options: [ModTypes],
                 items: [ModTypeCurrentValue],
-                placeholder: 'Paint Type',
+                placeholder: 'Mod Type',
                 labelField: 'name',
                 searchField: ['name'],
                 openOnFocus: true,
                 highlight: false,
                 hideSelected: true,
                 selectOnTab: true
-            });
-
-            //Alter the visibility settings dynamically via an onChange event
-            $(document).ready(function custVis() {
-                $('#select-ModType').on('change', function () {
-                    //These Paint Types only have X values
-                    if (this.value == 'Aluminium Polished' || this.value == 'Aluminium Semigloss' || this.value == 'Carbon Fibre Polished'
-                        || this.value == 'Chrome' || this.value == 'Matte' || this.value == 'Normal' || this.value == 'Semigloss') {
-                        document.getElementById('ModTypeXCondition').hidden = false;
-                        document.getElementById('colourX').required = true;
-                        document.getElementById('saturationX').required = true;
-                        document.getElementById('brightnessX').required = true;
-
-                        document.getElementById('ModTypeYCondition').hidden = true;
-                        document.getElementById('colourY').value = '';
-                        document.getElementById('colourY').required = false;
-                        document.getElementById('saturationY').value = '';
-                        document.getElementById('saturationY').required = false;
-                        document.getElementById('brightnessY').value = '';
-                        document.getElementById('brightnessY').required = false;
-                    }
-                    //Condition if a paint type isn't selected. Also an exception for this paint type that doesn't require an axis
-                    else if (this.value == 'Prismacolour White' || this.value.length === 0) {
-                        document.getElementById('ModTypeYCondition').hidden = true;
-                        document.getElementById('ModTypeXCondition').hidden = true;
-                        document.getElementById('colourX').value = '';
-                        document.getElementById('colourX').required = false;
-                        document.getElementById('saturationX').value = '';
-                        document.getElementById('saturationX').required = false;
-                        document.getElementById('brightnessX').value = '';
-                        document.getElementById('brightnessX').required = false;
-                        document.getElementById('colourY').value = '';
-                        document.getElementById('colourY').required = false;
-                        document.getElementById('saturationY').value = '';
-                        document.getElementById('saturationY').required = false;
-                        document.getElementById('brightnessY').value = '';
-                        document.getElementById('brightnessY').required = false;
-
-                    }
-                    //All other paint types have X and Y values for both axes
-                    else {
-                        document.getElementById('ModTypeYCondition').hidden = false;
-                        document.getElementById('ModTypeXCondition').hidden = false;
-                    }
-                });
-                //Ensures the correct fields are showing on popup load
-                $('#select-ModType').trigger('change');
             });
         }
     });
